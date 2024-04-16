6eae86a175eda4a3b2445fe0f414927243d280a0
Dane Osborne <dane3018@gmail.com>
2024-04-16 13:32:46 +1000

    added lots

--- util_functions.py
+++ util_functions.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import socket
 from socket import *
+import time
 
 
 # Sock Line functions 
@@ -23,25 +24,47 @@ def read_response(sock, is_binary):
     
     """
     # Read as bytes. Only convert to UTF-8 when we have entire line.
-    in_data = b''
-    while True:
-        ch = sock.recv(4096)
-        if len(ch) == 0:
-            # Socket closed. If we have any data it is an incomplete
-            # line, otherwise immediately return None
-            if len(in_data) > 0:
+    sock.settimeout(5)
+    start_time = time.time()
+    try:
+        in_data = b''
+        while True:
+            ch = sock.recv(4096)
+            if len(ch) == 0:
+                # Socket closed. If we have any data it is an incomplete
+                # line, otherwise immediately return None
+                if len(in_data) > 0:
+                    break
+                else:
+                    return None
+            in_data += ch
+            # this pattern indicates the end of the request
+            if not is_binary and ch.endswith(b'\r\n.\r\n'):
                 break
-            else:
+            # if receiving data for longer than 5 seconds
+            if time.time() - start_time > 5:
+                print('timeout')
+                sock.close()
                 return None
-        in_data += ch
-        # this pattern indicates the end of the request
-        if ch.endswith(b'\r\n.\r\n'):
-            break
-    # if is_binary:
-    #     return in_data
-    txt = in_data.decode('utf-8', 'backslashreplace')
-    sock.close()
+        sock.close()
+        if is_binary:
+            return in_data
+        txt = in_data.decode('utf-8', 'backslashreplace')
+        return remove_terminator(txt)
+    except Exception as e:
+        print("timeout:", e)
+        sock.close()
+        return None
+    
+def remove_terminator(txt):
+    # edge case where .txt file is empty no leading new line
+    if txt == ".\r\n":
+        return ""
+    suffix = "\r\n.\r\n"
+    if txt.endswith(suffix):
+        return txt[:-len(suffix)]
     return txt
+
 
 
 
@@ -61,11 +84,14 @@ def get_resources(txt):
     """
     if not txt:
         return None
+    # error code
+    # if txt.startswith('3'):
+    #     return [txt.split('\r\n')[0],]
     resources = txt.split('\r\n')
     result = []
 
     for res in resources:
-        if(res.startswith(tuple(str(i) for i in range(10)))):
+        if(res.startswith(('0','1','2','3','9'))):
             values = res.split('\t')
             
             if len(values) != 4:
@@ -78,11 +104,27 @@ def get_resources(txt):
                 'port': values[3]
             }
             result.append(current)
+        # if(res.startswith('3')):
+        #     current = {
+                
+        #     }
 
     return result
 
 
 
-def count_dirs(txt):
-    return 0
+
+
+
+def check_external_server(host_name, portno):
+        server_up = False
+        try:
+            sock = socket(AF_INET, SOCK_STREAM)
+            sock.settimeout(5)
+            sock.connect((host_name, portno))
+            server_up = True
+        finally:
+            sock.close()
+            return server_up
+
 
